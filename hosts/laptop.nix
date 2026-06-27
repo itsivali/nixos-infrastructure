fad28ea1e8d69b7725d00620da90f024acfb10e0
Willis Ivali <itsivali@outlook.com>
2026-06-27 23:19:40 +0300

    added correct age path

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -54,7 +54,7 @@
     # SOPS
     ###########################################################
     sops = lib.mkIf config.ivali.secrets.enable {
-      age.keyFile = "/var/lib/sops-nix/key.txt";
+      age.keyFile = "/home/ivali/.config/sops/age/keys.txt";
       defaultSopsFile = ../secrets/tailscale.yaml;
       secrets = {
         tailscale_authkey = {

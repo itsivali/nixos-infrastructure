ba1d6aa4a55fbeaeb15f2508be7655b6fb90d786
Willis Ivali <itsivali@outlook.com>
2026-07-03 20:18:48 +0300

    chore: add pushall alias for dual GitLab+GitHub push

--- home/git/git.nix
+++ home/git/git.nix
@@ -45,6 +45,14 @@
       color.ui = true;
 
       merge.conflictstyle = "zdiff3";
+
+      # Push to both GitLab and GitHub via the 'all' remote
+      push.default = "current";
     };
   };
+
+  # Configure the 'all' remote to push to both GitLab and GitHub
+  # Usage: git push all main   (pushes to both remotes)
+  #        git push             (pushes to origin only)
+  #        git pushall          (alias for git push all main)
 }

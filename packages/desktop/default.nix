9b321f8f5b5c3a164c250803dbd7f25a738e519d
Willis Ivali <itsivali@outlook.com>
2026-08-02 21:32:51 +0000

    added microsoft edge

--- packages/desktop/default.nix
+++ packages/desktop/default.nix
@@ -1,35 +1,34 @@
-##############################################################################
-#
+\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+
 # Packages Desktop
-#
+
 # Purpose
+
 # -------
+
 # Defines the set of desktop applications (Firefox, VLC, LibreOffice,
+
 # Obsidian, Zoom, etc.) included in the system package set.
-#
+
 # Ownership
+
 # ---------
-# Willis Ivali <ivali>
-#
+
+# Willis Ivali 
+
 # Responsibilities
+
 # ----------------
+
 # - Provide a curated list of desktop application packages
+
 # - Be importable by the system package aggregator
-#
-##############################################################################
+
+\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
 
 # Desktop applications — combined into system package set by aggregator.
-{ pkgs }:
 
-with pkgs; [
-  localsend
-  zoom-us
-  obsidian
-  mpv
-  firefox
-  libreoffice-fresh
-  brightnessctl
-  xdg-utils
-  mission-center
-  notion-app-enhanced
-]
+\{ pkgs \ }:
+
+with pkgs; \[ localsend zoom-us obsidian mpv firefox libreoffice-fresh brightnessctl xdg-utils mission-center notion-app-enhanced **microsoft-edge**\ ]
+
